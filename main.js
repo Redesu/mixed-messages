e747a5f45a30aa315011250523dc4a9ccace5292
Willyan David <62126803+Redesu@users.noreply.github.com>
2024-03-14 11:35:18 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,10 @@
+//Random SMT physical skill message generator
+
+const physicalSkills = ["Lunge", "Bestial Bite", "Hellish Slash", "Gram Slice", "Dream Needle", "Toxic Sting", "Critical Slash", "Madness Needle", "Aramasa", "Bouncing Claw", "Scratch Dance", "Berserker God", "Needle Spray", "Fang Breaker", "Pierce Armor", "Blinding Strike", "Heavy Blow", "Heat Wave", "Puncture Punch", "Venom Chaser", "Beatdown", "Blight", "Carnage Fang", "Crusher Onslaught", "Hysterical Slap", "Dream Fist", "Eat Whole", "Axel Claw", "Damascus Claw", "Critical Wave", "Dark Sword", "Acrobat Kick", "Fatal Sword", "Severing Bite", "Purple Smoke", "Steel Needle", "Frenzied Chomp", "Mighty Cleave", "Mortal Jihad", "Deathbound", "Rampage", "Mist Rush", "Wrath Tempest", "Madness Nails", "Nihil Claw", "Power Punch", "Hell Thrust", "Brave Blade", "Akashic Arts", "Mac an Luin", "Pandemonic Crush", "Megaton Press", "Yabusame Shot", "Figment Slash", "Myriad Slashes", "Hades Blast", "Titanomachia", "Hassou Tobi", "Catastrophe", "Karnak", "Gungnir", "Dancing Strike", "Astral Saintstrike", "Headcrush", "Somersault", "Panta Spane", "Andalucia", "Hell Spin", "Terrorblade", "Pestilence", "Javelin Rain", "Gaea Rage", "Deadly Fury"]
+
+// Every time a user runs a program, they should get a new, randomized output.
+
+const random = Math.floor(Math.random() * physicalSkills.length);
+
+
+console.log(physicalSkills[random]);
